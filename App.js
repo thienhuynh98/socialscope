ba44701cf5d76d38a5e15ee1e61fc83d46c20ddf
Gihane Ndjeuha <gihanen.ndjeuha@bellevuecollege.edu>
2021-11-16 12:18:27 -0800

    react commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  const element = <h1 style={{ color: 'purple' }}>Hello world</h1>
+
+    </div>
+  );
+}
+
+export default App;
